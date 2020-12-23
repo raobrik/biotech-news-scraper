9e8025e5d7cce4fc319c398578c23a06fe648f60
Brian Rao <brao4@illinois.edu>
2020-12-23 13:11:49 -0600

    search bar usage

--- scraper.py
+++ scraper.py
@@ -1,17 +1,34 @@
-import sys
-import os
-from selenium import webdriver
 
+from selenium import webdriver
 #pip install webdriver-manager
 from webdriver_manager.chrome import ChromeDriverManager
 
-#os.environ["PATH"] += os.pathsep + os.pathsep.join([r"C:\Program Files (x86)\chromedriver.exe"])
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
-#PATH = os.environ["PATH"]
-#$PATH += [r"C:\Users\raobr\Documents\Projects\MyProjects\Python\new_biotech_scraper\repo\webdriver\chromedriver_win32\chromedriver.exe"]
-#PATH = r"C:\Users\raobr\Documents\Projects\MyProjects\Python\new_biotech_scraper\repo\webdriver\chromedriver_win32\chromedriver.exe"
 
+
+import time 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get("https://www.biospace.com/news/")
+driver.get("https://www.genomeweb.com/")
+
+browser = driver
+browser.maximize_window()
+driver.implicitly_wait(5)
+
+#finds search bar via full xpath, else "element not interactable" 
+search = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div[3]/div/div[4]/div/div/form/div/div/div/div[1]/div/div/input")
+search.click()
+driver.implicitly_wait(5)
+
+#ActionChains(driver).move_to_element(search).send_keys("microbiome").send_keys(Keys.RETURN)
+#type query and hit enter
+search.send_keys("microbiome")
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
+
+driver.quit()
+
